b2175176a23eb728aa11293da9da4ec40da51de6
SAYED <sayedulsayed818@gmail.com>
2025-06-26 04:10:19 +0600

    add some file for learning basic

---
+++ 01_basic_Node/moduler_system/commonJs/file_1.js
@@ -0,0 +1,4 @@
+const {a,add,b}=require("./file_2.js");
+console.log(a);
+console.log(add(2,3));
+console.log(b);
---
+++ 01_basic_Node/moduler_system/commonJs/file_2.js
@@ -0,0 +1,8 @@
+const a=30;
+const add=(param1,param2)=>param1+param2;
+const b=20;
+module.exports={
+    a,
+    add,
+    b
+};
