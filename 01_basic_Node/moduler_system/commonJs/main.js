62d0e5a82a836e23fec3eecfcb7f5066993e100e
SAYED <sayedulsayed818@gmail.com>
2025-06-26 04:18:04 +0600

    we add some utils function and use them in main.js

---
+++ 01_basic_Node/moduler_system/commonJs/main.js
@@ -0,0 +1,3 @@
+const {add,subtract}=require("./utils");
+console.log(add(10,29));
+console.log(subtract(20,12));
---
+++ 01_basic_Node/moduler_system/commonJs/utils/subtract.js
@@ -0,0 +1,4 @@
+const subtract =(param1,param2)=>param1-param2;
+module.exports={
+    subtract
+}
